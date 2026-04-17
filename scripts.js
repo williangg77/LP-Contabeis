6bb7f73c2196535e71cc46142d1aadf790e92793
ws5410510-afk <ws5410510@gmail.com>
2026-04-17 20:03:20 -0300

    versao 1.0

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// Rolagem suave para links de navegação
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
+  });
+});
